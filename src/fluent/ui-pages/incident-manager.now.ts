fa8d92879ffceb946a2a1aa58a1b87534273963b
Denn Cayacap <denncayacap@gmail.com>
2026-04-21 23:33:16 +0800

    hotfix acadresolve 0.3

--- src/fluent/ui-pages/incident-manager.now.ts
+++ src/fluent/ui-pages/incident-manager.now.ts
@@ -4,7 +4,6 @@ import { UiPage } from '@servicenow/sdk/core'
 UiPage({
     $id: 'incident-manager-page',
     endpoint: 'x_1997678_acadreso_incident_manager.do',
-    description: 'Incident Response Manager UI Page',
+    description: 'AcadResolve Incident Manager',
     category: 'general',
-    direct: true,
 })
